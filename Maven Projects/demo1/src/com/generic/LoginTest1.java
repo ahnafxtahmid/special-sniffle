a1af09088d9a8228ba8a1ce0b90c2dabaa37c874
Tahmid <ahnaf.x.tahmid@gmai.com>
2024-10-28 22:43:42 -0400

    First

---
+++ Maven Projects/demo1/src/com/generic/LoginTest1.java
@@ -0,0 +1,16 @@
+public class LoginTest1 {
+
+public void getLogin(){
+
+System.out.println("This is a login example");
+
+}
+    
+public static void main(String[] args) {
+    
+LoginTest1 lt = new LoginTest1();
+lt.getLogin();
+
+}
+
+}
